4cf977e2632829d7620584bc29096b733f5188d7
Ethan Salonga <ethansalonga28@gmail.com>
2023-02-03 17:31:12 -0500

    init commit

---
+++ pages/about.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Head from "next/head"
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta
+          name="keywrords"
+          content="web development, programming"
+        />
+      </Head>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "@next/font/google"
+import styles from "@/styles/Home.module.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta
+          name="keywrords"
+          content="web development, programming"
+        />
+      </Head>
+
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
